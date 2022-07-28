e55750a5c483a11fba0ca385a0845694d629b4f3
Gaurav Yadav <gk5480325@gmail.com>
2022-07-28 13:13:42 +0530

    images and data js file done

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Layout({children, title}) {
   return (
@@ -9,10 +10,20 @@ export default function Layout({children, title}) {
         <meta name="description" content="ECommerce Website" />
         <link rel="icon" href="/favicon.ico" />
         </Head>
-        <div>
-        <header>Header</header>
-        <main>{children}</main>
-        <footer>Footer</footer>
+        <div className='flex min-h-screen flex-col justify-between'>
+        <header>
+          <nav className='flex h-12 items-center px-4 justify-between shadow-md'>
+            <Link href='/'>
+              <a className='text-lg font-bold'>AClue</a>
+            </Link>
+          <div>
+            <Link href='/cart'><a className='font-bold p-2'>Cart</a></Link>
+            <Link href='/login'><a className='font-bold p-2'>Login</a></Link>
+          </div>
+          </nav>
+        </header>
+        <main className='container m-auto mt-4 px-4'>{children}</main>
+        <footer className='flex h-10 justify-center items-center shadow-inner'>Copyright © 2022 AClue</footer>
         </div>
     </>
   )
